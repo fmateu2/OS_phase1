b905cc2ed30de628e83b83a869d6f20a3f2fdb27
jan porcar <janporcar30@gmail.com>
2026-03-08 20:45:33 +0100

    Update alliance.c

--- alliance.c
+++ alliance.c
@@ -1,27 +1,14 @@
 #include "alliance.h"
 #include "utils.h"
 
-Alliance *load_aliances(Config cfg) {
-    Alliance *alliances = malloc(sizeof(Alliance) * cfg.num_routes);
-    if (!alliances) {
-        printF("ERROR: Cannot allocate alliances.");
-        return NULL;
-    } else {
-        for (int i = 0; i < cfg.num_routes; i++) {
-            alliances[i].realm = strdup(cfg.routes[i].name);
-            alliances[i].alliance = PENDING;
-        }
-    }
-    return alliances;    
+Alliance *init_alliances(void) {
+    return NULL;
 }
 
-void free_alliances(Alliance *alliances, Config cfg){
-    if (!alliances) {
-        return;
-    } else {
-        for (int i = 0; i < cfg.num_routes; i++) {
-            free(alliances[i].realm);
-        }
-        free(alliances);
+void free_alliances(Alliance *alliances, int count) {
+    if (!alliances) return;
+    for (int i = 0; i < count; i++) {
+        free(alliances[i].realm);
     }
+    free(alliances);
 }
