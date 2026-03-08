3bbbe6e7021d23e5d95d4d97fdb01f698ff7de00
jan porcar <janporcar30@gmail.com>
2026-03-08 21:26:56 +0100

    Update utils.c

--- utils.c
+++ utils.c
@@ -1,9 +1,5 @@
 #include "utils.h"
 
-/**
- * @brief Reads characters from fd until separator is found.
- * @return Dynamically allocated string, or NULL on EOF/error
- */
 char *readUntil(int fd, char separator) {
     char ch;
     int i = 0;
@@ -42,23 +38,19 @@ char *readUntil(int fd, char separator) {
     return buffer;
 }
 
-/**
- * @brief Copies src into dst converting all chars to uppercase.
- */
+
 void str_toupper_copy(char *dst, const char *src, int maxlen) {
     int i;
     for (i = 0; i < maxlen - 1 && src[i] != '\0'; i++)
-        dst[i] = (char)toupper((unsigned char)src[i]);
+        dst[i] = (char)toupper((int)src[i]);
     dst[i] = '\0';
 }
 
-/**
- * @brief Trims leading and trailing whitespace in place.
- */
+
 void str_trim(char *s) {
     int start = 0, end;
-    while (s[start] != '\0' && isspace((unsigned char)s[start])) start++;
+    while (s[start] != '\0' && isspace((int)s[start])) start++;
     if (start > 0) memmove(s, s + start, strlen(s) - start + 1);
     end = (int)strlen(s) - 1;
-    while (end >= 0 && isspace((unsigned char)s[end])) s[end--] = '\0';
+    while (end >= 0 && isspace((int)s[end])) s[end--] = '\0';
 }
